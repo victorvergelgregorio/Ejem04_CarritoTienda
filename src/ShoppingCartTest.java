e03c92d04cacea4fa01bcb5ecbebaddb0f0a955c
victorvergelgregorio <victorvergel@gregoriofer.com>
2016-05-05 09:50:15 +0200

    Confirmo localmente los cambios

--- src/ShoppingCartTest.java
+++ src/ShoppingCartTest.java
@@ -1,6 +1,8 @@
 
-
 import java.util.ArrayList;
+
+
+
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,90 +11,92 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-
 @RunWith(value=Parameterized.class)
 public class ShoppingCartTest {
-
-	ShoppingCart carrito;
 	
-
-	public ShoppingCartTest(Product datosIniciales[],int numElemenAnadidos) {
+	private ShoppingCart carrito;
+	private Product[] productos;
+	private int elementos;
+	private double total;
+		
+	public ShoppingCartTest(Product[] datosiniciales, int numElementos,double total){
 		super();
+		carrito= new ShoppingCart();
+		productos= datosiniciales;
+		elementos= numElementos;
+		this.total=total;
+		for (int i = 0; i < productos.length; i++) {
+			carrito.addItem(productos[i]);
+		}
 
+		
 	}
 	
 	@Parameters
-	public static ArrayList<Object []> parametros()
-	{
-		ArrayList<Object []> parametrosDelConstructor=new ArrayList<Object []>();
-		Object [] primerTesteo=new Object [2];
-		Product p1=new Product("gorras1", 33);
-		Product p2=new Product("peines1", 34);
-		Product p3=new Product("peinetas1", 35);
-		Product productosIniciales[]=new Product[4];
-		productosIniciales[0]=p1;
-		productosIniciales[1]=p2;
-		productosIniciales[2]=p3;
-		primerTesteo[0]=productosIniciales;
-		primerTesteo[1]=3;
+	public static ArrayList<Object []> parametrosCarro(){
+		ArrayList<Object []> parametros = new ArrayList<Object[]>();
 		
-		return parametrosDelConstructor;
+		Product p1 = new Product("Zapatos", 33);
+		Product p2 = new Product("Zapatos2", 32);
+		Product p3 = new Product("Zapatos3", 35);
+		Product[] productos= new Product[3];
+		productos[0]= p1;
+		productos[1]= p2;
+		productos[2]= p3;		
 		
+		Object [] datos= new Object[3];
+		
+		datos[0]=productos;
+		datos[1]=3;
+		datos[2]=100.0;
+		
+		parametros.add(datos);
+		
+		return parametros;
 	}
-	
 
 	@Test
 	public void testGetBalance() {
 
+		Assert.assertEquals(total,carrito.getBalance(),0);
 	}
-	
-	@Before
-	public void inicializacionPersonalizadaPorMi()
-	{
-		carrito=new ShoppingCart();
-		Product p1=new Product("gorras", 33);
-		Product p2=new Product("peines", 34);
-		Product p3=new Product("peinetas", 35);
-		Product p4=new Product("zapatos", 35);
-		carrito.addItem(p1);
-		carrito.addItem(p2);
-		carrito.addItem(p3);
-		carrito.addItem(p4);
-	}
-	
+
 	@Test
 	public void testAddItem() {
-		Product p1=new Product("zapatillas", 33);
-		Product p2=new Product("zapatillas2", 34);
-		Product p3=new Product("zapatillas3", 35);
 		
-		int numProductosInicial=carrito.getItemCount();
-		carrito.addItem(p1);
-		carrito.addItem(p2);
-		carrito.addItem(p3);
-		int numProductosActual=carrito.getItemCount();
-		Assert.assertEquals(numProductosInicial+3, numProductosActual);
-		
+	}
+
+	@Test
+	public void testRemoveItem() {
+		System.out.println("numero elementos:"+carrito.getItemCount());
+		int unidadesAntes=carrito.getItemCount();
+		carrito.addItem(productos[0]);
 		try {
-			carrito.removeItem(p2);
+			carrito.removeItem(productos[0]);
 		} catch (ProductNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Assert.assertEquals(numProductosInicial+2, numProductosActual);
-		
-		
+		int unidadesDespues=carrito.getItemCount();
+		Assert.assertEquals(unidadesDespues,unidadesAntes);
+
+		carrito.addItem(productos[0]);
+		carrito.addItem(productos[0]);
+		try {
+			carrito.removeItem(productos[0]);
+		} catch (ProductNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		carrito.addItem(productos[0]);
+		Assert.assertEquals(unidadesDespues+2,unidadesAntes+2);
 	}
 	
-
-
-	
-
-	@Test
-	public void testRemoveItem() {
-
+	@Test(expected=ProductNotFoundException.class)
+	public void testRemoveItemExcepcion() throws ProductNotFoundException {
+		carrito.removeItem(new Product("Mesa", 22));
+		
 	}
-
 	@Test
 	public void testGetItemCount() {
 
@@ -105,7 +109,7 @@ public class ShoppingCartTest {
 
 	@Test
 	public void testIsEmpty() {
-
+		Assert.assertEquals(carrito.getItemCount(), 0);
 	}
 
 }
